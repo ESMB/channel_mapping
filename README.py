b8421b146882c454b9694dbd9ae78cd5bdd49c5c
orie1876 <65364777+orie1876@users.noreply.github.com>
2020-08-12 10:12:24 +0100

    First version.

---
+++ README.py
@@ -0,0 +1,5 @@
+Map channels recorded using optosplit on the TIRF microscope. 
+
+Requires an image of tetraspek beads in both channels. 
+
+Python requires imreg_dft installing (https://pypi.org/project/imreg_dft/).
